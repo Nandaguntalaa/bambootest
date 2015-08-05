87fcf96fe80cd2d8fe3e3066cd4e33347108cf1d
Nandaguntalaa <gncoded@gmail.com>
2015-08-04 23:17:13 -0700

    Sample Code

---
+++ Sample_Code.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        Sample_Code.py
+# Purpose:
+#
+# Author:      nguntalaa
+#
+# Created:     04/08/2015
+# Copyright:   (c) nguntalaa 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print "This is my sample code for CI purposes."
+
+if __name__ == '__main__':
+    main()
---
+++ first_test.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        first_test.py
+# Purpose:
+#
+# Author:      nguntalaa
+#
+# Created:     04/08/2015
+# Copyright:   (c) nguntalaa 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print "This is my first test"
+
+if __name__ == '__main__':
+    main()
